bab6f93146ac71dcdd73ba202110036ea6f56a3d
PowerOfNames <PowerOfNames@web.de>
2025-04-20 14:29:33 +0200

    added andbox project, linked with StarFire

---
+++ Sandbox/premake5.lua
@@ -0,0 +1,63 @@
+project "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++23"
+	staticruntime "off"
+
+	targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "sfpch.h"
+	pchsource "src/sfpch.cpp"
+	
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+	
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+	
+	includedirs
+	{
+		"src"
+	}
+	
+	links
+	{
+		"StarFire"
+	}
+	
+	
+	filter "system:windows"
+		systemversion "latest"
+		
+	filter "action:vs*"
+		buildoptions
+		{
+			"/utf-8"
+		}
+		characterset "Unicode"
+	
+	filter "configurations:Debug"
+		defines "SF_DEBUG"
+		runtime "Debug"
+		symbols "on"
+		
+		links
+		{			
+		}
+		
+	filter "configurations:Release"
+		defines "SF_RELEASE"
+		runtime "Release"
+		optimize "on"
+		
+		links
+		{
+		}
+		
+		
